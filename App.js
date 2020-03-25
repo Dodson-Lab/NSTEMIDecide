b6bb716d440a89acc46f9719b1446e9efa7a11cf
Anna <annas.kiefer@Annas-iMac.local>
2020-03-24 22:50:45 -0700

    Created app skeleton and first page

---
+++ App.js
@@ -0,0 +1,108 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+  listenOrientationChange as lor,
+  removeOrientationChange as rol
+} from 'react-native-responsive-screen';
+
+import FlipPage, {FlipPagePage} from 'react-native-flip-page';
+import Page1 from './src/components/Page1'
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+          <FlipPage orientation={'horizontal'}>
+            <FlipPagePage>
+              <Page1/>
+            </FlipPagePage>
+            <FlipPagePage>
+              <Text style={{color: 'blue'}}>Page 2</Text>
+            </FlipPagePage>
+          </FlipPage>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: '#E8E8E8',
+    height: hp('100%')
+  },
+  sectionContainer: {
+    marginTop: hp('15%'),
+    paddingHorizontal: 24,
+    flex: 1,
+  },
+  sectionTitle: {
+    fontSize: hp('3.5%'),
+    fontWeight: '600',
+    color: Colors.black,
+    fontFamily: 'Helvetica Neue',
+  },
+  sectionDescription: {
+    fontSize: hp('3.5%'),
+    fontWeight: '400',
+    color: Colors.black,
+    fontFamily: 'Helvetica Neue',
+  },
+  noteTitle: {
+    fontSize: hp('2%'),
+    fontWeight: '600',
+    color: Colors.black,
+    fontFamily: 'Helvetica Neue',
+  },
+  noteDescription: {
+    fontSize: hp('2%'),
+    fontWeight: '400',
+    color: Colors.black,
+    fontFamily: 'Helvetica Neue',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  // footer: {
+  //   color: Colors.dark,
+  //   fontSize: 12,
+  //   fontWeight: '600',
+  //   padding: 4,
+  //   paddingRight: 12,
+  //   textAlign: 'right',
+  // },
+});
+
+export default App;
---
+++ src/components/Page1.js
@@ -0,0 +1,122 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+  listenOrientationChange as lor,
+  removeOrientationChange as rol
+} from 'react-native-responsive-screen';
+
+class Page1 extends Component {
+    render() {
+        return (
+            <SafeAreaView>
+            <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+            <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Cardiac catheterization:</Text>
+                <Text style={styles.sectionDescription}>
+                Is this procedure right for you?
+                </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>A Decision Aid Tool</Text>
+                <Text style={styles.sectionDescription}>
+                for older patients (age ≥75) considering cardiac catheterization as
+                treatment for a heart attack
+                </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+                <Text style={styles.noteTitle}>Note for clinicians:</Text>
+                <Text style={styles.noteDescription}>
+                This decision aid is designed for patients with Non-ST elevation MI (NSTEMI) only.
+                </Text>
+            </View>
+            </View>
+            </ScrollView>
+            </SafeAreaView>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: '#E8E8E8',
+    height: hp('100%')
+  },
+  sectionContainer: {
+    marginTop: hp('15%'),
+    paddingHorizontal: 24,
+    flex: 1,
+  },
+  sectionTitle: {
+    fontSize: hp('3.5%'),
+    fontWeight: '600',
+    color: Colors.black,
+    fontFamily: 'Helvetica Neue',
+  },
+  sectionDescription: {
+    fontSize: hp('3.5%'),
+    fontWeight: '400',
+    color: Colors.black,
+    fontFamily: 'Helvetica Neue',
+  },
+  noteTitle: {
+    fontSize: hp('2%'),
+    fontWeight: '600',
+    color: Colors.black,
+    fontFamily: 'Helvetica Neue',
+  },
+  noteDescription: {
+    fontSize: hp('2%'),
+    fontWeight: '400',
+    color: Colors.black,
+    fontFamily: 'Helvetica Neue',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  // footer: {
+  //   color: Colors.dark,
+  //   fontSize: 12,
+  //   fontWeight: '600',
+  //   padding: 4,
+  //   paddingRight: 12,
+  //   textAlign: 'right',
+  // },
+});
+
+export default Page1;
